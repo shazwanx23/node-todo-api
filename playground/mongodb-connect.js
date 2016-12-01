40128c0d74f2cebc303728b19034e97b126039d0
Nor Shazwan Shah Nor Hisham Shah <shazwan.hisham@mimos.local>
2016-12-01 15:19:34 +0800

    objectId and config file

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,16 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+/// put in mongo.config file
+// dbpath=D:\Users\shazwan.hisham\mongo-data\
+//run
+//mongod.exe --config /path/to/config/file
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+//
+// var user = { name: 'shazwan', age: 25};
+// var {name} = user;
+// console.log(name);
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if(err) {
@@ -17,15 +29,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   // });
 
   //name, age, location
-  db.collection('Users').insertOne({
-    name: 'Shazwan',
-    age: 23,
-    location: 'Seri Kembangan'
-  }, (err, result) => {
-    if(err){
-      return console.log('Unable to insert user', err);
-    }
-    console.log(JSON.stringify(result.ops, undefined, 2));
-  });
+  // db.collection('Users').insertOne({
+  //   name: 'Shazwan',
+  //   age: 23,
+  //   location: 'Seri Kembangan'
+  // }, (err, result) => {
+  //   if(err){
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+  // });
   db.close();
 });
