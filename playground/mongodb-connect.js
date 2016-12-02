da05a19e27c1c34d8dcfd02e6cd7453a38a3164c
Nor Shazwan Shah Nor Hisham Shah <shazwan.hisham@mimos.local>
2016-12-02 10:04:58 +0800

    deleting from mongo

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -17,16 +17,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     return console.log('Unable to connect to MongoDB server.');
   }
   console.log('Connected to MongoDb server');
-  // db.collection('Todos').insertOne({
-  //   text: 'Something to do',
-  //   completed: false
-  // }, (err, result) => {
-  //   if(err){
-  //     return console.log('Unable to insert todo', err);
-  //   }
-  //
-  //   console.log(JSON.stringify(result.ops, undefined, 2));
-  // });
+  db.collection('Todos').insertOne({
+    text: 'Something to do',
+    completed: false
+  }, (err, result) => {
+    if(err){
+      return console.log('Unable to insert todo', err);
+    }
+
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
 
   //name, age, location
   // db.collection('Users').insertOne({
