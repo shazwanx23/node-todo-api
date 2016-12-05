88558f7cbf594008fee4487fc89d06748550a181
shazwan <shazwanx23@gmail.com>
2016-12-05 19:51:53 +0800

    Setup test database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.promise = global.Promise;
 //heroku mongo ''
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 // mongoose.connect('mongodb://heroku_g7t4k15b:nofeeroactj4rk3asr8d8bc4f6@ds119618.mlab.com:19618/heroku_g7t4k15b' || 'mongodb://localhost:27017/TodoApp');
 // mongodb://heroku_g7t4k15b:heroku123password@ds119618.mlab.com:19618/heroku_g7t4k15b
 module.exports = { mongoose };
