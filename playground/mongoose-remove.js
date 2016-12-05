95408b61df0fd97959db05a6a61ab86f9c559efc
Nor Shazwan Shah Nor Hisham Shah <shazwan.hisham@mimos.local>
2016-12-05 12:18:23 +0800

    Add delete todos by id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,16 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+//Todo.findOneAndRemove
+//Todo.findByIdAndRemove
+
+Todo.findByIdAndRemove('5844e7352fbd9c3b19d552e4').then((todo) => {
+
+})
