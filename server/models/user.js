d67da1aa4eed60685e13428d50987386e049b9b0
Nor Shazwan Shah Nor Hisham Shah <shazwan.hisham@mimos.local>
2016-12-08 15:00:53 +0800

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _  = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -58,7 +59,7 @@ UserSchema.statics.findByToken = function(token) {
   try{
     decoded = jwt.verify(token, 'abc123');
   } catch (e) {
-    return Promise.reject(); 
+    return Promise.reject();
   }
 
   return User.findOne({
@@ -66,8 +67,23 @@ UserSchema.statics.findByToken = function(token) {
     'tokens.token' : token,
     'tokens.access' : 'auth'
   });
-
 };
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  if(user.isModified('password')) {
+      //user.password
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user.password, salt, (err, hash) => {
+          user.password = hash;
+          next();
+        });
+      });
+  }else {
+    next();
+  }
+});
 
 var User = mongoose.model('User', UserSchema);
 
